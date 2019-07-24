f5ae1a8d170a33158947172f0cad3a938852955e
徐壮壮 <xuzhuangzhuang@cmhi.chinamobile.com>
2019-07-24 16:16:09 +0800

    branch commit 2

--- GitTestProject/ViewController.m
+++ GitTestProject/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     NSLog(@"branch commit 1");
+    NSLog(@"branch commit 2");
 }
 
 
